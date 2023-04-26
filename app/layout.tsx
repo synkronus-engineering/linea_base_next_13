e19a84d2ac9c649e4a5d0ae3c1791fb799bf5afc
synkronus <synkronus.software@gmail.com>
2023-04-26 14:03:00 -0500

    feat: added progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import SupabaseListener from '@/src/providers/SupabaseListener';
 import SupabaseProvider from '@/src/providers/SupabaseProvider';
 import '@/src/styles/demo/Demos.scss';
 import '@/src/styles/layout/layout.scss';
+import 'nprogress/nprogress.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.min.css';
--- src/@page-sections/layouts/LayoutWrapper.tsx
+++ src/@page-sections/layouts/LayoutWrapper.tsx
@@ -1,10 +1,27 @@
 'use client';
-import { ReactNode } from 'react';
+import '@/src/styles/nprogress.css';
+import { usePathname, useSearchParams } from 'next/navigation';
+import nProgress from 'nprogress';
+import { ReactNode, useEffect } from 'react';
 import AppTopbar from './AppTopbar';
+
+nProgress.configure({ showSpinner: false });
 
 type WrapperProps = { children: ReactNode };
 
 const LayoutWrapper = ({ children }: WrapperProps) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    (() => {
+      nProgress.start();
+      setTimeout(() => {
+        nProgress.done();
+      }, 500);
+    })();
+  }, [pathname, searchParams]);
+
   return (
     <>
       <AppTopbar />
