f502c067f120e601e38bec058651a027bdf7a0aa
JG <synkronus.software@gmail.com>
2023-08-03 21:05:15 -0500

    feat: caching strategy

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,6 +1,11 @@
 import BlogDetail from '@/src/@page-sections/blog/BlogArticle';
 import { supabaseClient } from '@/src/lib/supabase';
 
+export async function generateStaticParams() {
+  const productList = await supabaseClient().from('blog').select(`id`);
+  return productList.data;
+}
+
 async function fetchData(id: any) {
   return supabaseClient()
     .from('blog')
